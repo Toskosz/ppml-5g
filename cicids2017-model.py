bd8ca82370be41853705ec8eb40de72eb6adb69d
Tokarski <thiago.tokarski2001@gmail.com>
2025-07-31 11:10:23 -0300

    test nem sample size

--- cicids2017-model.py
+++ cicids2017-model.py
@@ -113,13 +113,14 @@ y_train = train_df['binary_label']
 y_test = test_df['binary_label']
 
 sample_size = 40000
+test_sample_size = 0.2 * sample_size
 if X_train.shape[0] > sample_size:
     np.random.seed(42) # for reproducibility
     indices = np.random.choice(X_train.shape[0], sample_size, replace=False)
     X_train_sampled = X_train[indices]
     y_train_sampled = y_train.iloc[indices]
 
-    test_indices = np.random.choice(X_test.shape[0], sample_size, replace=False)
+    test_indices = np.random.choice(X_test.shape[0], test_sample_size, replace=False)
     X_test_sampled = X_test[test_indices]
     y_test_sampled = y_test.iloc[test_indices]
 else:
