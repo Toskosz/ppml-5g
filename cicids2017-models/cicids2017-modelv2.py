72ab9ef030b952364e96a39dfe5f4907ca88964a
Tokarski <thiago.tokarski2001@gmail.com>
2025-08-01 20:21:50 -0300

    train esti2 depth3

--- cicids2017-models/cicids2017-modelv2.py
+++ cicids2017-models/cicids2017-modelv2.py
@@ -168,8 +168,8 @@ if X_train_final is not X_train_dense_reduced:
 if X_test_final is not X_test_dense_reduced:
     del X_test_dense_reduced
 
-n_estimators_list = [2, 5, 10]
-max_depth_list = [4, 4, 4]
+n_estimators_list = [2]
+max_depth_list = [3]
 
 for n_estimators, max_depth in zip(n_estimators_list, max_depth_list):
     print("\n" + "="*60)
