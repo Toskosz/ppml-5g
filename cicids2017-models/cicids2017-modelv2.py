f312222378dc227d74724506ee9707f312854f32
Tokarski <thiago.tokarski2001@gmail.com>
2025-08-02 23:19:31 -0300

    fix

--- cicids2017-models/cicids2017-modelv2.py
+++ cicids2017-models/cicids2017-modelv2.py
@@ -118,7 +118,7 @@ with open('preprocessor_cic_kdd_equivalent.pkl', 'wb') as f:
 
 
 # --- Dimensionality Reduction using TruncatedSVD ---
-n_components_svd = 200 # <-- CRITICAL: Adjust this value
+n_components_svd = 100 # <-- CRITICAL: Adjust this value
 print(f"\nApplying TruncatedSVD to reduce feature count from {X_train_sparse.shape[1]} to {n_components_svd}...")
 
 svd = TruncatedSVD(n_components=n_components_svd, random_state=42)
@@ -169,7 +169,7 @@ if X_test_final is not X_test_dense_reduced:
     del X_test_dense_reduced
 
 n_estimators_list = [2]
-max_depth_list = [3]
+max_depth_list = [4]
 
 for n_estimators, max_depth in zip(n_estimators_list, max_depth_list):
     print("\n" + "="*60)
